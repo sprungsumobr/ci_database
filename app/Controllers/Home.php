f9eb30207aa2e08ffb089614322c1c90ec3d4aad
sprungsumobr <48767935+sprungsumobr@users.noreply.github.com>
2022-04-06 15:02:51 -0300

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,8 +2,11 @@
 
 namespace App\Controllers;
 
+use app\Models\PessoasModel;
+
 class Home extends BaseController
 {
+
     public function index()
     {
         echo view('template/header');
@@ -16,4 +19,17 @@ class Home extends BaseController
         echo view($page);
         echo view('template/footer');
     }
+
+    public function pessoa(){
+        $model = new PessoaModel();
+
+        $data = [
+            'title'=>'Pessoas',
+            'pessoas'=>$model->getPessoas()
+        ];
+        echo view('template/header');
+        echo view('pessoa',$data);
+        echo view('template/footer');
+    }
+
 }
